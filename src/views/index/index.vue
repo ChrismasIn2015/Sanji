724dae4db8e7d0911a4ae9960a4cb3e4cb982fec
Wqao <974879409@qq.com>
2020-04-08 18:11:28 +0800

    auto update

--- src/views/index/index.vue
+++ src/views/index/index.vue
@@ -17,7 +17,45 @@ export default {
     toLibrary() {
       this.$router.push({ name: "library" });
     },
-    leetCode() {}
+    leetCode() {
+      // 1.
+      function MyPromise(excutor) {
+        var self = this;
+        self.onResolvedCallback = [];
+        function resolve(value) {
+          setTimeout(() => {
+            self.data = value;
+            self.onResolvedCallback.forEach(callback => callback(value));
+          });
+        }
+        excutor(resolve.bind(self));
+      }
+      MyPromise.prototype.then = function(onResolved) {
+        var self = this;
+        // 一定要返回一个新的promise
+        // promise2
+        var async2 = resolve => {
+          console.log("执行then的同步代码");
+          self.onResolvedCallback.push(function() {
+            var result = onResolved(self.data);
+            result instanceof Promise ? result.then(resolve) : resolve(result);
+          });
+        };
+
+        return new MyPromise(async2);
+      };
+      // 2.
+      function myAsync(resolve) {
+        setTimeout(() => {
+          console.log("执行用户的异步代码");
+          resolve("async 500");
+        }, 500);
+      }
+      var tt = new MyPromise(myAsync).then(res => {
+        console.log("then res");
+      });
+      console.log("sync 0");
+    }
   }
 };
 </script>
