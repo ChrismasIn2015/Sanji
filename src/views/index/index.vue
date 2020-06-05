ca512779de3635c0a36a979c558842e6b1ded0fa
HuangWenQiang <974879409@qq.com>
2020-06-05 19:50:56 +0800

    auto push from lvmama

--- src/views/index/index.vue
+++ src/views/index/index.vue
@@ -2,18 +2,14 @@
   <div class="index">
     <div class="index-entry flex">
       <div class="entry common-btn" @click.stop="toLibrary">笔记系统</div>
-      <div class="entry common-btn blue" @click.stop="toHistory">
-        历史大事件
-      </div>
+      <div class="entry common-btn blue" @click.stop="toHistory">历史大事件</div>
       <div class="entry common-btn orange" @click.stop="toUI">sjUI</div>
       <div class="entry common-btn pink" @click.stop="toColor">Color</div>
       <div
         class="entry common-btn toggle"
         style="margin-left: auto;"
         @click.stop="openMyDevTool"
-      >
-        打开控制台
-      </div>
+      >打开控制台</div>
     </div>
 
     <!-- 每日标记 -->
@@ -23,91 +19,91 @@
 </template>
 
 <script>
-  import planBoard from './components/planBoard.vue'
-  export default {
-    data() {
-      return {
-        myScheduleA: [
-          {
-            time: null,
-            tip: 'Start: 0515',
-            affair: ['sjUI', 'erpUI', 'WebServer C++'],
-            style: { 'background-color': 'rgb(255, 125, 212)' },
-          },
-          {
-            time: null,
-            affair: [
-              '计算机组成原理',
-              '操作系统',
-              '内存 & C++',
-              'WebServe & C++',
-              '32bit ≈ 43Y',
-              '64bit ≈ 1844YY',
-            ],
-            style: { 'background-color': 'rgb(235, 255, 125)' },
-          },
-        ],
-        myScheduleB: [
-          {
-            time: ['07:00', '09:00'],
-            affair: ['Politics', 'Calculus'],
-            style: { 'background-color': 'rgb(255, 125, 212)' },
-          },
-          {
-            time: ['10:00', '12:00'],
-            affair: ['Politics', 'Calculus'],
-            style: { 'background-color': 'rgb(235, 255, 125)' },
-          },
-          {
-            time: ['14:00', '17:00'],
-            affair: ['Computer Science'],
-            style: { 'background-color': 'rgb(125, 229, 255)' },
-          },
-          {
-            time: ['21:00', '00:00'],
-            affair: ['Comprehension', 'Writing'],
-            style: { 'background-color': 'rgb(125, 255, 194)' },
-          },
-          {
-            time: ['09:00', '20:00'],
-            affair: [
-              'A1.（√√）中台UI=1920',
-              'B1.（xx）点击+登录判断',
-              'B2.（xx）点击+小程序',
-              'B31.（x√）功能链接+小红点消息中心',
-              'B32.（xx）功能链接+扫描二维码',
-              'B41.（xx）图文导航+圆角',
-              'B42.（xx）图文导航+背景图',
-              'B5.（xx）图片广告+是否轮播',
-              'B6.（x√）Tab资讯+样式更改',
-            ],
-            style: { 'background-color': 'rgb(125, 255, 194)' },
-          },
-        ],
-      }
+import planBoard from "./components/planBoard.vue";
+export default {
+  data() {
+    return {
+      myScheduleA: [
+        {
+          time: null,
+          tip: "Start: 0515",
+          affair: ["sjUI", "erpUI", "WebServer C++"],
+          style: { "background-color": "rgb(255, 125, 212)" }
+        },
+        {
+          time: null,
+          affair: [
+            "计算机组成原理",
+            "操作系统",
+            "内存 & C++",
+            "WebServe & C++",
+            "32bit ≈ 43Y",
+            "64bit ≈ 1844YY"
+          ],
+          style: { "background-color": "rgb(235, 255, 125)" }
+        }
+      ],
+      myScheduleB: [
+        {
+          time: ["07:00", "09:00"],
+          affair: ["Politics", "Calculus"],
+          style: { "background-color": "rgb(255, 125, 212)" }
+        },
+        {
+          time: ["10:00", "12:00"],
+          affair: ["Politics", "Calculus"],
+          style: { "background-color": "rgb(235, 255, 125)" }
+        },
+        {
+          time: ["14:00", "17:00"],
+          affair: ["Computer Science"],
+          style: { "background-color": "rgb(125, 229, 255)" }
+        },
+        {
+          time: ["21:00", "00:00"],
+          affair: ["Comprehension", "Writing"],
+          style: { "background-color": "rgb(125, 255, 194)" }
+        },
+        {
+          time: ["09:00", "20:00"],
+          affair: [
+            "A1.（√√）中台UI=1920",
+            "B1.（x√）点击+登录判断",
+            "B2.（xx）点击+小程序",
+            "B31.（x√）功能链接+小红点消息中心",
+            "B32.（x√）功能链接+扫描二维码",
+            "B41.（xx）图文导航+圆角",
+            "B42.（xx）图文导航+背景图",
+            "B5.（xx）图片广告+是否轮播",
+            "B6.（x√）Tab资讯+样式更改"
+          ],
+          style: { "background-color": "rgb(125, 255, 194)" }
+        }
+      ]
+    };
+  },
+  components: {
+    planBoard
+  },
+  methods: {
+    toUI() {
+      this.$router.push({ name: "uiBoard" });
     },
-    components: {
-      planBoard,
+    toLibrary() {
+      this.$router.push({ name: "library" });
     },
-    methods: {
-      toUI() {
-        this.$router.push({ name: 'uiBoard' })
-      },
-      toLibrary() {
-        this.$router.push({ name: 'library' })
-      },
-      toHistory() {
-        this.$router.push({ name: 'history' })
-      },
-      toColor() {
-        this.$router.push({ name: 'colorBoard' })
-      },
-      openMyDevTool() {
-        let ipc = require('electron').ipcRenderer
-        ipc.send('openDevTool')
-      },
+    toHistory() {
+      this.$router.push({ name: "history" });
     },
+    toColor() {
+      this.$router.push({ name: "colorBoard" });
+    },
+    openMyDevTool() {
+      let ipc = require("electron").ipcRenderer;
+      ipc.send("openDevTool");
+    }
   }
+};
 </script>
 
 <style lang="scss" scoped>
--- src/views/library/components/richText.vue
+++ src/views/library/components/richText.vue
@@ -2,82 +2,97 @@
   <div class="rich">
     <div class="rich-nav flex">
       <div class="nav-item" @click.stop="selectBlod">Blod</div>
-      <div class="nav-item" style="color: red;" @click.stop="selectColor('red')">Red</div>
-      <div class="nav-item" style="color: #4169E1;" @click.stop="selectColor('#4169E1')">Blue</div>
+      <div
+        class="nav-item"
+        style="color: red;"
+        @click.stop="selectColor('red')"
+      >
+        Red
+      </div>
+      <div
+        class="nav-item"
+        style="color: #4169E1;"
+        @click.stop="selectColor('#4169E1')"
+      >
+        Blue
+      </div>
     </div>
     <div class="rich-content">
-      <div id="richText" contenteditable="true" v-html="content" @keydown="keyListen"></div>
+      <div id="richText" contenteditable="true" @keydown="keyListen">
+        {{ content }}
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-export default {
-  props: {
-    width: { type: String, default: "auto" },
-    height: { type: String, default: "auto" },
-    content: { type: String, default: "" }
-  },
-  data() {
-    return {};
-  },
-  methods: {
-    selectBlod() {
-      document.execCommand("ForeColor", false, "black");
-      document.execCommand("Bold");
+  export default {
+    props: {
+      width: { type: String, default: 'auto' },
+      height: { type: String, default: 'auto' },
+      content: { type: String, default: '' },
     },
-    selectColor(color) {
-      document.execCommand("ForeColor", false, color);
-      document.execCommand("Bold");
+    data() {
+      return {}
     },
-    getContent() {
-      let target = document.getElementById("richText");
-      let content = target.innerHTML;
-      return content;
+    methods: {
+      selectBlod() {
+        document.execCommand('ForeColor', false, 'black')
+        document.execCommand('Bold')
+      },
+      selectColor(color) {
+        document.execCommand('ForeColor', false, color)
+        document.execCommand('Bold')
+      },
+      getContent() {
+        let target = document.getElementById('richText')
+        let content = target.innerHTML
+        console.log(target, content)
+        return content
+      },
+      keyListen() {
+        // 1.监听tab
+        if (event.keyCode === 9) {
+          document.execCommand('insertText', false, '    ')
+        }
+      },
     },
-    keyListen() {
-      // 1.监听tab
-      if (event.keyCode === 9) {
-        document.execCommand("insertText", false, "    ");
-      }
-    }
   }
-};
 </script>
 
 <style lang="scss" scoped>
-.rich {
-  background-color: $common-tip-lower;
-  padding: 0.5rem;
-  margin-top: 0.5rem;
-  max-width: 100%;
-  .rich-nav {
-    padding-bottom: 0.5rem;
-    border-bottom: 1px solid $common-tip;
-    .nav-item {
-      margin-right: 0.5rem;
-      text-align: center;
-      cursor: pointer;
-      line-height: 2rem;
-      // width: 2rem;
-      height: 2rem;
-      padding: 0rem 0.5rem;
-      background-color: $common-tip;
-      &:hover {
-        background-color: $common-white;
+  .rich {
+    background-color: $common-tip-lower;
+    padding: 0.5rem;
+    margin-top: 0.5rem;
+    max-width: 100%;
+    .rich-nav {
+      padding-bottom: 0.5rem;
+      border-bottom: 1px solid $common-tip;
+      .nav-item {
+        margin-right: 0.5rem;
+        text-align: center;
+        cursor: pointer;
+        line-height: 2rem;
+        // width: 2rem;
+        height: 2rem;
+        padding: 0rem 0.5rem;
+        background-color: $common-tip;
+        &:hover {
+          background-color: $common-white;
+        }
+      }
+    }
+    .rich-content {
+      overflow-y: auto;
+      word-wrap: break-word;
+      max-height: 43.6rem;
+      #richText {
+        padding: 0.5rem 0rem;
+        &:focus {
+          outline: 0rem;
+        }
       }
     }
   }
-  .rich-content {
-    overflow-y: auto;
-    word-wrap: break-word;
-    max-height: 43.6rem;
-    #richText {
-      padding: 0.5rem 0rem;
-      &:focus {
-        outline: 0rem;
-      }
-    }
-  }
-}
 </style>
