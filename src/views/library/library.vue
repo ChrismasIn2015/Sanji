7875d00b686445ca50fcc835356b5a077355217c
HuangWenQiang <974879409@qq.com>
2020-07-16 18:07:46 +0800

    auto push from lvmama

--- src/views/library/library.vue
+++ src/views/library/library.vue
@@ -11,20 +11,11 @@
           @click.stop="setNowBlockId(item)"
           :class="{ 'tip-on': nowBlockId === item.blockId }"
         >
-          <span
-            class="pot"
-            v-show="nowBlockId === item.blockId"
-            @click.stop="modalToConfirm('block')"
-          >✖</span>
-          <span
-            class="pot pot-edit"
-            v-show="nowBlockId === item.blockId"
-            @click.stop="editBlockShow = true"
-          >··</span>
+          <span class="pot" v-show="nowBlockId === item.blockId" @click.stop="modalToConfirm('block')">✖</span>
+          <span class="pot pot-edit" v-show="nowBlockId === item.blockId" @click.stop="editBlockShow = true">··</span>
           <span>{{ item.name }}</span>
         </div>
         <div class="common-btn" @click.stop="newBlockShow = true">+</div>
-        <div class="nav-tip" v-show="!blockList.length">暂无数据</div>
       </div>
     </div>
 
@@ -34,21 +25,9 @@
         <!-- 书架 -->
         <div class="shelfs">
           <div class="shelf" v-for="(item, index) in shelfList" :key="index">
-            <div
-              class="common-btn blue-on"
-              @click.stop="setNowShelfId(item)"
-              :class="{ blue: nowShelfId === item.shelfId }"
-            >
-              <span
-                class="pot"
-                v-show="nowShelfId === item.shelfId"
-                @click.stop="modalToConfirm('shelf')"
-              >✖</span>
-              <span
-                class="pot pot-edit"
-                v-show="nowShelfId === item.shelfId"
-                @click.stop="editShelfShow = true"
-              >..</span>
+            <div class="common-btn blue-on" @click.stop="setNowShelfId(item)" :class="{ blue: nowShelfId === item.shelfId }">
+              <span class="pot" v-show="nowShelfId === item.shelfId" @click.stop="modalToConfirm('shelf')">✖</span>
+              <span class="pot pot-edit" v-show="nowShelfId === item.shelfId" @click.stop="editShelfShow = true">..</span>
               <span>{{ item.name }}（{{ item.books.length }}）</span>
             </div>
 
@@ -63,16 +42,8 @@
                 }"
                 @click.stop="setNowBookId(book)"
               >
-                <span
-                  class="pot"
-                  v-show="nowBookId === book.bookId"
-                  @click.stop="modalToConfirm('book')"
-                >✖</span>
-                <span
-                  class="pot pot-edit"
-                  v-show="nowBookId === book.bookId"
-                  @click.stop="editBookShow = true"
-                >··</span>
+                <span class="pot" v-show="nowBookId === book.bookId" @click.stop="modalToConfirm('book')">✖</span>
+                <span class="pot pot-edit" v-show="nowBookId === book.bookId" @click.stop="editBookShow = true">··</span>
                 <span>{{ book.name }}</span>
               </div>
               <div class="common-btn orange" @click.stop="newBookShow = true">+</div>
@@ -103,11 +74,7 @@
       <div class="common-modal-blank" style=" max-width: 30rem;">
         <div class="modal-title">添加区块</div>
         <div class="modal-content">
-          <sjInput
-            ref="myBlock"
-            :state="{ type: 'none', holder: '请输入区块名称' }"
-            style="margin-top: 1rem;"
-          />
+          <sjInput ref="myBlock" :state="{ type: 'none', holder: '请输入区块名称' }" style="margin-top: 1rem;" />
         </div>
         <div class="modal-btns">
           <div class="common-btn" @click.stop="createMyBlock">添加</div>
@@ -120,11 +87,7 @@
       <div class="common-modal-blank" style=" max-width: 30rem;">
         <div class="modal-title">编辑区块</div>
         <div class="modal-content">
-          <sjInput
-            ref="myBlock"
-            :state="{ type: 'none', holder: '请输入新区块名称' }"
-            style="margin-top: 1rem;"
-          />
+          <sjInput ref="myBlock" :state="{ type: 'none', holder: '请输入新区块名称' }" style="margin-top: 1rem;" />
         </div>
         <div class="modal-btns">
           <div class="common-btn" @click.stop="editMyBlock">修改</div>
@@ -137,11 +100,7 @@
       <div class="common-modal-blank" style=" max-width: 30rem;">
         <div class="modal-title">添加书架</div>
         <div class="modal-content">
-          <sjInput
-            ref="myShelf"
-            :state="{ type: 'none', holder: '请输入书架名称' }"
-            style="margin-top: 1rem;"
-          />
+          <sjInput ref="myShelf" :state="{ type: 'none', holder: '请输入书架名称' }" style="margin-top: 1rem;" />
         </div>
         <div class="modal-btns">
           <div class="common-btn" @click.stop="createMyShelf">添加</div>
@@ -154,11 +113,7 @@
       <div class="common-modal-blank" style=" max-width: 30rem;">
         <div class="modal-title">编辑书架</div>
         <div class="modal-content">
-          <sjInput
-            ref="myShelf"
-            :state="{ type: 'none', holder: '请输入新书架名称' }"
-            style="margin-top: 1rem;"
-          />
+          <sjInput ref="myShelf" :state="{ type: 'none', holder: '请输入新书架名称' }" style="margin-top: 1rem;" />
         </div>
         <div class="modal-btns">
           <div class="common-btn" @click.stop="editMyShelf">修改</div>
@@ -171,11 +126,7 @@
       <div class="common-modal-blank" style=" max-width: 30rem;">
         <div class="modal-title">添加书籍</div>
         <div class="modal-content">
-          <sjInput
-            ref="myBook"
-            :state="{ type: 'none', holder: '请输入书籍名称' }"
-            style="margin-top: 1rem;"
-          />
+          <sjInput ref="myBook" :state="{ type: 'none', holder: '请输入书籍名称' }" style="margin-top: 1rem;" />
         </div>
         <div class="modal-btns">
           <div class="common-btn" @click.stop="createMyBook">添加</div>
@@ -188,11 +139,7 @@
       <div class="common-modal-blank" style=" max-width: 30rem;">
         <div class="modal-title">编辑书籍</div>
         <div class="modal-content">
-          <sjInput
-            ref="myBook"
-            :state="{ type: 'none', holder: '请输入新书名称' }"
-            style="margin-top: 1rem;"
-          />
+          <sjInput ref="myBook" :state="{ type: 'none', holder: '请输入新书名称' }" style="margin-top: 1rem;" />
         </div>
         <div class="modal-btns">
           <div class="common-btn" @click.stop="editMyBookName">修改</div>
@@ -204,188 +151,188 @@
 </template>
 
 <script>
-import indexEntry from "@/components/indexEntry/indexEntry.vue";
-import sjInput from "@/utils/sanJi-UI/sjInput.vue";
-import richText from "./components/richText";
-import {
-  block_create,
-  getBlockList,
-  block_editName,
-  block_remove,
-  shelf_create,
-  getShelfList,
-  shelf_editName,
-  shelf_remove,
-  book_create,
-  book_editName,
-  book_remove,
-  book_getContent,
-  book_setContent
-} from "@/utils/lowDB/control/libraryAPI.js";
+  import indexEntry from '@/components/indexEntry/indexEntry.vue'
+  import sjInput from '@/utils/sanJi-UI/sjInput.vue'
+  import richText from './components/richText'
+  import {
+    block_create,
+    getBlockList,
+    block_editName,
+    block_remove,
+    shelf_create,
+    getShelfList,
+    shelf_editName,
+    shelf_remove,
+    book_create,
+    book_editName,
+    book_remove,
+    book_getContent,
+    book_setContent,
+  } from '@/utils/lowDB/control/libraryAPI.js'
 
-export default {
-  data() {
-    return {
-      // 选中区块
-      blockList: [],
-      nowBlockId: "",
-      newBlockShow: false,
-      editBlockShow: false,
-      // 选中书架
-      shelfList: [],
-      nowShelfId: "",
-      newShelfShow: false,
-      editShelfShow: false,
-      // 选中书籍
-      nowBookId: "",
-      nowBookContent: "",
-      newBookShow: false,
-      editBookShow: false
-    };
-  },
-  mounted() {
-    this.getMyBlockList();
-  },
-  components: {
-    sjInput,
-    richText,
-    indexEntry
-  },
-  methods: {
-    // ************************************************ 区块
-    createMyBlock() {
-      if (this.$refs.myBlock.inputVerify()) return;
-      let blockName = this.$refs.myBlock.inputValue;
-      block_create(blockName);
-      this.getMyBlockList();
-      this.$tip("添加成功");
-      this.newBlockShow = false;
+  export default {
+    data() {
+      return {
+        // 选中区块
+        blockList: [],
+        nowBlockId: '',
+        newBlockShow: false,
+        editBlockShow: false,
+        // 选中书架
+        shelfList: [],
+        nowShelfId: '',
+        newShelfShow: false,
+        editShelfShow: false,
+        // 选中书籍
+        nowBookId: '',
+        nowBookContent: '',
+        newBookShow: false,
+        editBookShow: false,
+      }
     },
-    editMyBlock() {
-      if (this.$refs.myBlock.inputVerify()) return;
-      let blockName = this.$refs.myBlock.inputValue;
-      block_editName(this.nowBlockId, blockName);
-      this.getMyBlockList();
-      this.editBlockShow = false;
+    mounted() {
+      this.getMyBlockList()
     },
-    getMyBlockList() {
-      let list = getBlockList();
-      this.blockList = Object.assign([], list);
-      // this.$test.log("取得区块列表", list);
+    components: {
+      sjInput,
+      richText,
+      indexEntry,
     },
-    setNowBlockId(item) {
-      this.clearLibrary();
-      this.nowBlockId = item.blockId;
-      this.getMyShelfList();
+    methods: {
+      // ************************************************ 区块
+      createMyBlock() {
+        if (this.$refs.myBlock.inputVerify()) return
+        let blockName = this.$refs.myBlock.inputValue
+        block_create(blockName)
+        this.getMyBlockList()
+        this.$tip('添加成功')
+        this.newBlockShow = false
+      },
+      editMyBlock() {
+        if (this.$refs.myBlock.inputVerify()) return
+        let blockName = this.$refs.myBlock.inputValue
+        block_editName(this.nowBlockId, blockName)
+        this.getMyBlockList()
+        this.editBlockShow = false
+      },
+      getMyBlockList() {
+        let list = getBlockList()
+        this.blockList = Object.assign([], list)
+        // this.$test.log("取得区块列表", list);
+      },
+      setNowBlockId(item) {
+        this.clearLibrary()
+        this.nowBlockId = item.blockId
+        this.getMyShelfList()
+      },
+      deleteMyBlock() {
+        block_remove(this.nowBlockId)
+        this.clearLibrary()
+        this.getMyBlockList()
+        this.shelfList = []
+      },
+      // ************************************************ 书架
+      createMyShelf() {
+        if (this.$refs.myShelf.inputVerify()) return
+        let shelfName = this.$refs.myShelf.inputValue
+        shelf_create(this.nowBlockId, shelfName)
+        this.getMyShelfList()
+        this.$tip('添加成功')
+        this.newShelfShow = false
+      },
+      editMyShelf() {
+        if (this.$refs.myShelf.inputVerify()) return
+        let shelfName = this.$refs.myShelf.inputValue
+        shelf_editName(this.nowShelfId, shelfName)
+        this.getMyShelfList()
+        this.editShelfShow = false
+      },
+      getMyShelfList() {
+        let list = getShelfList(this.nowBlockId)
+        this.shelfList = Object.assign([], list)
+        console.log('取得书架列表', list)
+      },
+      setNowShelfId(item) {
+        this.nowBookId = ''
+        this.nowShelfId = item.shelfId
+      },
+      deleteMyShelf() {
+        shelf_remove(this.nowShelfId)
+        this.nowShelfId = ''
+        this.getMyShelfList()
+      },
+      // ************************************************ 3.书籍
+      createMyBook() {
+        if (this.$refs.myBook.inputVerify()) return
+        let bookName = this.$refs.myBook.inputValue
+        book_create(this.nowBlockId, this.nowShelfId, bookName, '')
+        this.getMyShelfList()
+        this.$tip('添加成功')
+        this.newBookShow = false
+      },
+      editMyBookName() {
+        if (this.$refs.myBook.inputVerify()) return
+        let bookName = this.$refs.myBook.inputValue
+        book_editName(this.nowBookId, bookName)
+        this.getMyShelfList()
+        this.editBookShow = false
+      },
+      setNowBookId(item) {
+        this.nowBookId = item.bookId
+        let content = book_getContent(item.bookId)
+        this.nowBookContent = content
+      },
+      deleteMyBook() {
+        book_remove(this.nowBookId)
+        this.nowBookId = ''
+        this.getMyShelfList()
+      },
+      // 取得富文本内容 并且保存
+      getMyRichText(data) {
+        book_setContent(this.nowBookId, data)
+        this.$tip('保存成功')
+      },
+      // ************************************************ 全局
+      clearLibrary() {
+        this.nowBlockId = ''
+        this.nowShelfId = ''
+        this.nowBookId = ''
+        this.nowBookContent = ''
+      },
+      toIndex() {
+        this.$router.push({ name: 'index' })
+      },
+      modalToConfirm(key) {
+        let option = {
+          title: '确认',
+          content: '确认要删除吗',
+        }
+        if (key === 'book')
+          this.$confirm(option, (res) => {
+            if (res) {
+              this.deleteMyBook()
+              this.$tip('删除成功')
+            }
+          })
+        if (key === 'shelf')
+          this.$confirm(option, (res) => {
+            if (res) {
+              this.deleteMyShelf()
+              this.$tip('删除成功')
+            }
+          })
+        if (key === 'block')
+          this.$confirm(option, (res) => {
+            if (res) {
+              this.deleteMyBlock()
+              this.$tip('删除成功')
+            }
+          })
+      },
     },
-    deleteMyBlock() {
-      block_remove(this.nowBlockId);
-      this.clearLibrary();
-      this.getMyBlockList();
-      this.shelfList = [];
-    },
-    // ************************************************ 书架
-    createMyShelf() {
-      if (this.$refs.myShelf.inputVerify()) return;
-      let shelfName = this.$refs.myShelf.inputValue;
-      shelf_create(this.nowBlockId, shelfName);
-      this.getMyShelfList();
-      this.$tip("添加成功");
-      this.newShelfShow = false;
-    },
-    editMyShelf() {
-      if (this.$refs.myShelf.inputVerify()) return;
-      let shelfName = this.$refs.myShelf.inputValue;
-      shelf_editName(this.nowShelfId, shelfName);
-      this.getMyShelfList();
-      this.editShelfShow = false;
-    },
-    getMyShelfList() {
-      let list = getShelfList(this.nowBlockId);
-      this.shelfList = Object.assign([], list);
-      console.log("取得书架列表", list);
-    },
-    setNowShelfId(item) {
-      this.nowBookId = "";
-      this.nowShelfId = item.shelfId;
-    },
-    deleteMyShelf() {
-      shelf_remove(this.nowShelfId);
-      this.nowShelfId = "";
-      this.getMyShelfList();
-    },
-    // ************************************************ 3.书籍
-    createMyBook() {
-      if (this.$refs.myBook.inputVerify()) return;
-      let bookName = this.$refs.myBook.inputValue;
-      book_create(this.nowBlockId, this.nowShelfId, bookName, "");
-      this.getMyShelfList();
-      this.$tip("添加成功");
-      this.newBookShow = false;
-    },
-    editMyBookName() {
-      if (this.$refs.myBook.inputVerify()) return;
-      let bookName = this.$refs.myBook.inputValue;
-      book_editName(this.nowBookId, bookName);
-      this.getMyShelfList();
-      this.editBookShow = false;
-    },
-    setNowBookId(item) {
-      this.nowBookId = item.bookId;
-      let content = book_getContent(item.bookId);
-      this.nowBookContent = content;
-    },
-    deleteMyBook() {
-      book_remove(this.nowBookId);
-      this.nowBookId = "";
-      this.getMyShelfList();
-    },
-    // 取得富文本内容 并且保存
-    getMyRichText(data) {
-      book_setContent(this.nowBookId, data);
-      this.$tip("保存成功");
-    },
-    // ************************************************ 全局
-    clearLibrary() {
-      this.nowBlockId = "";
-      this.nowShelfId = "";
-      this.nowBookId = "";
-      this.nowBookContent = "";
-    },
-    toIndex() {
-      this.$router.push({ name: "index" });
-    },
-    modalToConfirm(key) {
-      let option = {
-        title: "确认",
-        content: "确认要删除吗"
-      };
-      if (key === "book")
-        this.$confirm(option, res => {
-          if (res) {
-            this.deleteMyBook();
-            this.$tip("删除成功");
-          }
-        });
-      if (key === "shelf")
-        this.$confirm(option, res => {
-          if (res) {
-            this.deleteMyShelf();
-            this.$tip("删除成功");
-          }
-        });
-      if (key === "block")
-        this.$confirm(option, res => {
-          if (res) {
-            this.deleteMyBlock();
-            this.$tip("删除成功");
-          }
-        });
-    }
   }
-};
 </script>
 
 <style lang="scss" scoped>
-@import "./styles/library.scss";
+  @import './styles/library.scss';
 </style>
