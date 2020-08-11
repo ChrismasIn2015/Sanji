245c5ca6c22e943f02d32f3d8984ab63d9ced6dd
HuangWenQiang <974879409@qq.com>
2020-08-11 21:55:22 +0800

    auto push from lvmama

--- src/ipc/ipc_Invoker.js
+++ src/ipc/ipc_Invoker.js
@@ -1,27 +1,27 @@
 export default class Invoker {
-  vuePointer = null
-  // ============================ 构造函数
-  constructor(vuePointer) {
-    this.vuePointer = vuePointer
-  }
+    vuePointer = null
+    // ============================ 构造函数
+    constructor(vuePointer) {
+        this.vuePointer = vuePointer
+    }
 
-  // ============================ 方法区
-  // 打开控制台
-  async openDevTool() {
-    try {
-      let ipc = require('electron').ipcRenderer
-      await ipc.send('openDevTool')
-    } catch (error) {
-      this.vuePointer.$tip(error)
+    // ============================ 方法区
+    // 打开控制台
+    async openDevTool() {
+        try {
+            let ipc = require('electron').ipcRenderer
+            ipc.send('openDevTool')
+        } catch (error) {
+            this.vuePointer.$tip(error)
+        }
     }
-  }
-  // 打开浏览器
-  async openBrower(url) {
-    try {
-      let ipc = require('electron').ipcRenderer
-      ipc.send('openDefaultBrower', { url: url })
-    } catch (error) {
-      this.vuePointer.$tip(error.message)
+    // 打开浏览器
+    async openBrower(url) {
+        try {
+            let ipc = require('electron').ipcRenderer
+            ipc.send('openDefaultBrower', { url: url })
+        } catch (error) {
+            this.vuePointer.$tip(error.message)
+        }
     }
-  }
 }
