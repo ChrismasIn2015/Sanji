f7d133209fe55f951d16dbed83d87c2a359f0b8a
HuangWenQiang <974879409@qq.com>
2020-08-11 10:20:12 +0800

    auto push from lvmama

--- src/background.js
+++ src/background.js
@@ -7,29 +7,29 @@ import { createProtocol } from 'vue-cli-plugin-electron-builder/lib'
 
 let mainWindow = null
 function createWindow() {
-  mainWindow = new BrowserWindow({
-    width: 1280,
-    height: 720,
-    resizable: false,
-    webPreferences: {
-      nodeIntegration: true, // 这行代码表示允许使用 node API
-    },
-  })
-  Menu.setApplicationMenu(null) // 关闭顶部窗口
+    mainWindow = new BrowserWindow({
+        width: 1280,
+        height: 720,
+        resizable: false,
+        webPreferences: {
+            nodeIntegration: true, // 这行代码表示允许使用 node API
+        },
+    })
+    Menu.setApplicationMenu(null) // 关闭顶部窗口
 
-  // 根据环境加载SPA
-  let devURL = process.env.WEBPACK_DEV_SERVER_URL
-  if (devURL) {
-    // localhost:8080
-    mainWindow.loadURL(devURL)
-  } else {
-    createProtocol('app')
-    mainWindow.loadURL('app://./index.html')
-  }
+    // 根据环境加载SPA
+    let devURL = process.env.WEBPACK_DEV_SERVER_URL
+    if (devURL) {
+        // localhost:8080
+        mainWindow.loadURL(devURL)
+    } else {
+        createProtocol('app')
+        mainWindow.loadURL('app://./index.html')
+    }
 
-  mainWindow.on('closed', () => {
-    mainWindow = null
-  })
+    mainWindow.on('closed', () => {
+        mainWindow = null
+    })
 }
 
 // ********************************** 2.使用 protocol 模块保证浏览器正确加载本地资源（vue...electron-builder）
@@ -43,11 +43,11 @@ import './ipc/ipc.js'
 import { app } from 'electron'
 
 app.on('window-all-closed', () => {
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
 })
 
 app.on('ready', async () => {
-  createWindow()
+    createWindow()
 })
